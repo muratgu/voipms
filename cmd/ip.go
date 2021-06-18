792b484661c00b7f95868861e0f80dc6693db12a
muratgu <mgungora@gmail.com>
2021-06-17 18:29:13 -0700

    read config file

--- cmd/ip.go
+++ cmd/ip.go
@@ -25,7 +25,6 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// ipCmd represents the ip command
 var ipCmd = &cobra.Command{
 	Use:   "ip",
 	Short: "Get the client IP address",
@@ -37,14 +36,4 @@ var ipCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(ipCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// ipCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// ipCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/sms.go
+++ cmd/sms.go
@@ -29,7 +29,6 @@ import (
 
 var smsFrom string
 
-// smsCmd represents the sms command
 var smsCmd = &cobra.Command{
 	Use:   "sms [to] [message]",
 	Short: "Send an SMS",
@@ -53,15 +52,4 @@ var smsCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(smsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// smsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// smsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-
 }
